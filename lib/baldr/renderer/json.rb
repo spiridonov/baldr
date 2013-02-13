0d4ba6a37d32baaab3d29ec764852ac1367feddf
Stanislav Spiridonov <stanislav@spiridonov.pro>
2013-02-13 14:35:19 +0400

    json renderer

--- lib/baldr/renderer/json.rb
+++ lib/baldr/renderer/json.rb
@@ -1,5 +1,16 @@
-module Baldr::Renderer
-  class Json
+module Baldr::Renderer::Json
 
+  extend self
+
+  def draw(segment, params = {})
+    draw_segment(segment).to_json
   end
+
+  def draw_segment(segment)
+    node = ActiveSupport::OrderedHash.new
+    segment.elements.each.with_index { |e, i| node["#{segment.id}#{'%02d' % i}"] = e if e.present? }
+    segment.children.each { |loop| node[loop.id] = loop.segments.map { |s| draw_segment(s) } }
+    node
+  end
+
 end
